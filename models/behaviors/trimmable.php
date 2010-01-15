ab67131dada72fce9b2332d8106740a2e8a823e6
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-01-15 16:14:45 -0500

    simplifying code a bit for speed

--- models/behaviors/trimmable.php
+++ models/behaviors/trimmable.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Tr.im Behavior
+ * Tr.immable Behavior
  * 
  * Automatically saves a short url to the current record
  *
@@ -9,7 +9,7 @@
  * @version 0.1
  * @copyright 2009 Jose Diaz-Gonzalez
  **/
-class TrimBehavior extends ModelBehavior {
+class TrimmableBehavior extends ModelBehavior {
 
 	var $__settings = array();
 
@@ -25,13 +25,9 @@ class TrimBehavior extends ModelBehavior {
 			$this->__settings[$model->alias] = $default;
 		}
 
+		$settings = (is_array($settings)) ? $settings : array();
 		$this->__settings[$model->alias] = array_merge(
-			$this->__settings[$model->alias],
-			ife(
-				is_array($settings),
-				$settings,
-				array()
-			)
+			$this->__settings[$model->alias], $settings
 		);
 		
 	}
@@ -40,17 +36,16 @@ class TrimBehavior extends ModelBehavior {
 		$return = parent::beforeSave($model);
 
 		// What is the field that should hold this?
-		if(isset($model->data[$model->alias][$model->primaryKey]) && !empty($model->data[$model->alias][$model->primaryKey])) {
-			if (($this->__settings[$model->alias]['mode'] == 'update') || ($this->__settings[$model->alias]['mode'] == 'auto')) {
-				$field = $this->__settings[$model->alias]['field'];
-				if (!$model->hasField($field)) {
-				} else {
-					//Build the URL...
-					$controllerName = Inflector::tableize($model->alias);
-					$controllerName = Inflector::variable($controllerName);
-					$url = Router::url(array('controller' => $controllerName, 'action' => $this->__settings[$model->alias]['action'], $model->data[$model->alias][$model->primaryKey]));
-					$model->data[$model->alias][$field] = $this->trimURL($url, $this->__settings[$model->alias]['api']);
-				}
+		if(in_array($this->__settings[$model->alias]['mode'], array('update', 'auto')) 
+		and isset($model->data[$model->alias][$model->primaryKey])
+		and !empty($model->data[$model->alias][$model->primaryKey])) {
+			$field = $this->__settings[$model->alias]['field'];
+			if ($model->hasField($field)) {
+				//Build the URL...
+				$controllerName = Inflector::tableize($model->alias);
+				$controllerName = Inflector::variable($controllerName);
+				$url = Router::url(array('controller' => $controllerName, 'action' => $this->__settings[$model->alias]['action'], $model->data[$model->alias][$model->primaryKey]));
+				$model->data[$model->alias][$field] = $this->trimURL($url, $this->__settings[$model->alias]['api']);
 			}
 		}
 		return $return;
@@ -59,19 +54,16 @@ class TrimBehavior extends ModelBehavior {
 	public function afterSave(&$model, $created) {
 		$return = parent::afterSave($model, $created);
 
-		if(isset($model->id) && !empty($model->id)) {
-			if ($this->__settings[$model->alias]['mode'] == 'create') {
-				$field = $this->__settings[$model->alias]['field'];
-				if (!$model->hasField($field)) {
-				} else {
-					//Build the URL...
-					$controllerName = Inflector::tableize($model->alias);
-					$controllerName = Inflector::variable($controllerName);
-					$url = Router::url(array('controller' => $controllerName, 'action' => $this->__settings[$model->alias]['action'], $model->data[$model->alias][$model->primaryKey]));
-					$trimmedURL = $this->trimURL($url, $this->__settings[$model->alias]['api']);
-					$fieldToSaveTo = $model->alias . $field;
-					$model->saveField($fieldToSaveTo, $trimmedURL, true);
-				}
+		if(($this->__settings[$model->alias]['mode'] == 'create') and isset($model->id) and !empty($model->id)) {
+			$field = $this->__settings[$model->alias]['field'];
+			if ($model->hasField($field)) {
+				//Build the URL...
+				$controllerName = Inflector::tableize($model->alias);
+				$controllerName = Inflector::variable($controllerName);
+				$url = Router::url(array('controller' => $controllerName, 'action' => $this->__settings[$model->alias]['action'], $model->data[$model->alias][$model->primaryKey]));
+				$trimmedURL = $this->trimURL($url, $this->__settings[$model->alias]['api']);
+				$fieldToSaveTo = $model->alias . $field;
+				$model->saveField($fieldToSaveTo, $trimmedURL, true);
 			}
 		}
 		return $return;
